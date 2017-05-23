c0c265c159530c2424a14ae7ea653c933e7b19b3
xiaobo <1968468227@qq.com>
2017-05-23 02:38:47 -0700

    Moved 'fortune cookies' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,9 @@
 var express=require('express');
 var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
 
-var app=express();
+var fortune=require('./lib/fortune.js');
 
-var fortunes=[
-	"Conquer your fears or they will conquer you",
-	"Rivers need springs",
-	"Do not fear what you dont know",
-	"You will have a pleasant surprise",
-	"Whenever possible , keep it simple",
-];
+var app=express();
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
@@ -23,8 +17,7 @@ app.get('/',function(req,res){
 })
 
 app.get('/about',function(req,res){
-	var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about',{fortune:randomFortune});
+	res.render('about',{fortune:fortune.getFortune()});
 });
 
 //404 page
